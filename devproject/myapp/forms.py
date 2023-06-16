8e31b12d70b6f5a7c99c045bc2e0b1267a330568
hieppro114 <81932733+hieppro114@users.noreply.github.com>
2023-06-16 10:33:39 +0700

    Add files via upload

---
+++ devproject/myapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms  
+from myapp.models import Employee  
+class EmployeeForm(forms.ModelForm):  
+    class Meta:  
+        model = Employee  
+        fields = ['name', 'title', 'number_of_page', 'description', 'author', 'created_date'] 
+        widgets = { 
+            'name': forms.TextInput(attrs={ 'class': 'form-control' }), 
+            'title': forms.TextInput(attrs={ 'class': 'form-control' }),
+            'number_of_page': forms.NumberInput(attrs={ 'class': 'form-control' }),
+            'description': forms.TextInput(attrs={ 'class': 'form-control' }),
+            'author': forms.TextInput(attrs={ 'class': 'form-control' }),
+            'created_date': forms.TextInput(attrs={ 'class': 'form-control' }),
+      }
---
+++ devproject/myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):  
+    name = models.CharField(max_length=100)  
+    title = models.CharField(max_length=100)  
+    number_of_page = models.DecimalField(max_digits=15, decimal_places=10)
+    description = models.CharField(max_length=100)  
+    author = models.CharField(max_length=100)
+    created_date = models.CharField(max_length=100)
+   
+    class Meta:  
+        db_table = "tblemployee"
