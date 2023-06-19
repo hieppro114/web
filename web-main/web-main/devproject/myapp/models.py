e9e7d89a5176d902b62e0683c85287ee3667f21c
hieppro114 <hiepngotuan@gmail.com>
2023-06-19 11:08:56 +0700

    Done

---
+++ web-main/web-main/devproject/myapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+# Create your models here.
+
+
+class Employee(models.Model):
+    id = models.DecimalField(primary_key=True, max_digits=15, decimal_places=10)
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    number_of_page = models.DecimalField(max_digits=15, decimal_places=10)
+    description = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    created_date = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "tblemployee"
